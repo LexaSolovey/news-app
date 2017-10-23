3ff843e913789060779df2aa01845f33a4516d91
LexaSolovey <xoxach5@gmail.com>
2017-10-23 08:07:06 +0300

    update

--- client/src/index.js
+++ client/src/index.js
@@ -8,9 +8,7 @@ import { createStore } from 'redux';
 
 import reducer from './reducers';
 
-const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-
-const store = createStore(reducer, reduxDevTools);
+const store = createStore(reducer);
 
 ReactDOM.render(
   <Provider store={store}>
